0438f235fcbe3bcaa2d55991235cd91a0fb00a05
Cagdas Erman Afacan <afacanerman@gmail.com>
2015-10-25 11:58:43 +0000

    removed debugtoolbar

--- cdPython/urls.py
+++ cdPython/urls.py
@@ -11,9 +11,3 @@ urlpatterns = [
     url(r'', include('cd.urls'))
 ]
 
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += patterns('',
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
